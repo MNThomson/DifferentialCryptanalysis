a12198a0689907d5af467862788a95bce702e479
Max Thomson <git@mnt.dev>
2024-07-28 17:39:50 -0700

    Found second differential characteristic

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,8 @@ struct Characteristic {
 }
 impl Characteristic {
     /// We can ignore the mix subkeys step as per the paper
-    fn simplified_enc_first_3_rounds(plaintext: u16) -> u16 {
-        //
-        let mut text = plaintext;
-        for _ in 0..3 {
-            text = substitute(text);
-            text = permute(text);
-        }
-        text
+    fn simplified_enc_first_3_rounds(input: u16) -> u16 {
+        (0..3).fold(input, |data, _| permute(substitute(data)))
     }
 
     /// Reference: `4.3 Constructing Differential Characteristics`
@@ -37,7 +31,6 @@ impl Characteristic {
             let u2 = Self::simplified_enc_first_3_rounds(p2);
 
             let delta_u = u1 ^ u2;
-
             if du == delta_u {
                 right += 1;
             } else {
@@ -53,7 +46,7 @@ impl Characteristic {
 }
 
 /// Reference: `4.3 Constructing Differential Characteristics`
-fn find_characteristic() -> Characteristic {
+fn find_characteristic(offset: usize) -> Characteristic {
     let mut c = Characteristic {
         dp: 0,
         du: 0,
@@ -61,10 +54,10 @@ fn find_characteristic() -> Characteristic {
     };
 
     for delta_p in 1..0xF {
-        let delta_p = (delta_p << 8) as u16;
+        let delta_p: u16 = delta_p << 8;
 
         for delta_u in 0..0xFF {
-            let delta_u = (((delta_u & 0xF0) << 4) | (delta_u & 0xF)) as u16;
+            let delta_u: u16 = ((delta_u & 0xF0) << (4 + offset)) | ((delta_u & 0xF) << offset);
 
             let new_c = Characteristic::probability(delta_p, delta_u);
             if new_c.probability > c.probability {
@@ -76,6 +69,10 @@ fn find_characteristic() -> Characteristic {
 }
 
 fn main() {
-    let ca = find_characteristic();
-    println!("Differential Characteristic A: {:?}", ca);
+    let ca = find_characteristic(0);
+    let cb = find_characteristic(4);
+    println!(
+        "Differential Characteristics:\n\tA: {:?}\n\tB: {:?}",
+        ca, cb
+    );
 }
