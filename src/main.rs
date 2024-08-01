174727d0a292ef24e01a554bc4cae52077dd4f42
Hal <haln_01@proton.me>
2024-07-31 17:46:44 -0700

    feat: extract partial subkey

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ struct Characteristic {
     /// Probability
     probability: f64,
 }
+
 impl Characteristic {
     /// We can ignore the mix subkeys step as per the paper
     fn simplified_enc_first_3_rounds(input: u16) -> u16 {
@@ -128,6 +129,47 @@ mod attack {
         return keys;
     }
 
+    pub fn extract_partial_subkey(
+        cipher: &cipher::Cipher,
+        subkeys: &[u16; 256],
+        c: &Characteristic,
+    ) -> u16 {
+        let mut tracker = [0 as u16; 256];
+        let mut rng = rand::thread_rng();
+
+        for key_idx in 0..256 {
+            let subkey = subkeys[key_idx];
+
+            for _ in 0..ITERATIONS {
+                let p1: u16 = rng.gen();
+                let p2 = p1 ^ c.dp;
+
+                let c1 = cipher.full_encrypt(p1);
+                let c2 = cipher.full_encrypt(p2);
+
+                let u1 = cipher.partial_decrypt(c1, subkey);
+                let u2 = cipher.partial_decrypt(c2, subkey);
+
+                let _delta_c = c1 ^ c2;
+                let delta_u = u1 ^ u2;
+
+                if delta_u == c.du {
+                    tracker[key_idx] += 1;
+                }
+            }
+        }
+
+        // find the sub key with max counter.
+        let mut max_counter: u16 = 0;
+        let mut max_idx: usize = 0;
+        for (idx, &el) in tracker.iter().enumerate() {
+            if el > max_counter {
+                max_counter = el;
+                max_idx = idx;
+            }
+        }
+
+        return subkeys[max_idx];
     }
 }
 
@@ -140,10 +182,10 @@ fn main() {
 
     // extracting subkeys
     let subkey1 = attack::subkeys_generator(0);
-    let subkey1 = attack::extract_subkey(&cipher, &subkey1, &ca);
+    let subkey1 = attack::extract_partial_subkey(&cipher, &subkey1, &ca);
 
     let subkey2 = attack::subkeys_generator(4);
-    let subkey2 = attack::extract_subkey(&cipher, &subkey2, &cb);
+    let subkey2 = attack::extract_partial_subkey(&cipher, &subkey2, &cb);
 
     // concat and compare
     let subkey = subkey1 | subkey2;
